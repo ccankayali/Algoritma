2a8009c9e9498ccf75ab6c358df3df9fcee214f7
ccankayali <volkand135@gmail.com>
2024-05-13 16:02:58 +0300

    Final

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+// app.module.ts
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { WordModule } from './word/word.module';
+import { CorsMiddleware } from './cors.middleware'; // CORS middleware'ini ekleyin
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/Algoritma'),
+    AuthModule,
+    WordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CorsMiddleware) // CORS middleware'i uygula
+      .forRoutes({ path: '*', method: RequestMethod.ALL }); // Tüm istekler için CORS başlıklarını ekle
+  }
+}
---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { SignUpDto } from "./dto/signup.dto";
+import { AuthService } from "./auth.service";
+import { SignInDto } from "./dto/signin.dto";
+
+@Controller('/auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+    
+    @Post('/signup')
+    async signUp(@Body() signUpDto: SignUpDto) {
+        return await this.authService.signUp(signUpDto);
+    }
+    
+    @Post('/signin')
+    async signIn(@Body() signInDto: SignInDto) {
+        return await this.authService.signIn(signInDto);
+    }
+
+    @Post('/logout')
+    async logout() {
+        return await this.authService.logout();
+    }
+
+    @Post('/forgot-password')
+    async forgotPassword(@Body() forgotPasswordDto: SignUpDto) {
+        return await this.authService.forgotPassword(forgotPasswordDto);
+    }
+}
