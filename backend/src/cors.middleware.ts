604b50f698512cb1d7693578487b416d4319d481
ccankayali <volkand135@gmail.com>
2024-05-13 16:03:04 +0300

    Final

---
+++ backend/src/cors.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', '*'); // Herkese izin ver
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    // OPTIONS isteğine yanıt vermek için
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }
+}
